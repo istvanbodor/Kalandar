77d2f3e80953905e9f03d425770c486756fd7291
Hoós Roland <shadeeygg@gmail.com>
2023-03-01 14:24:03 +0100

    Starting working with dates, fixing graphics

--- Desktop/src/main/java/wv/kalandar/kalandar_projekt/CalendarApplication.java
+++ Desktop/src/main/java/wv/kalandar/kalandar_projekt/CalendarApplication.java
@@ -1,14 +1,22 @@
 package wv.kalandar.kalandar_projekt;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarApplication extends Application {
+    long lastRefreshTime = 0;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalendarApplication.class.getResource("calendar-view.fxml"));
@@ -18,9 +26,77 @@ public class CalendarApplication extends Application {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+
+        datumGeneralas();
     }
 
     public static void main(String[] args) {
         launch();
     }
+
+private Parent root;
+    public void datumGeneralas() throws IOException {
+
+        Date date = new Date();
+        System.out.println(date);
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        System.out.println(sdf.format(date));
+
+        Integer dateYear = Integer.parseInt(sdf.format(date).split("/")[0]);
+        System.out.println(dateYear);
+
+        Integer dateMonth = Integer.parseInt(sdf.format(date).split("/")[1]);
+        System.out.println(dateMonth);
+
+        Integer dateDay = Integer.parseInt(sdf.format(date).split("/")[2]);
+        System.out.println(dateDay);
+        System.out.println();
+
+        Integer lastDayOfMonth = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
+        System.out.println(lastDayOfMonth);
+
+        Calendar firstDayOfMonth = Calendar.getInstance();
+
+
+        String[] days = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        System.out.println(firstDayOfMonth.get(Calendar.DAY_OF_WEEK)-1);
+
+        Integer firstDay = firstDayOfMonth.get(Calendar.DAY_OF_WEEK)-1;
+
+        naptarDatum(firstDay, lastDayOfMonth);
+    }
+
+    public void initialize() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("calendar-view.fxml"));
+        root = loader.load();
+
+        SceneController sceneController = loader.getController();
+        sceneController.changeButtonText("13");
+    }
+
+    public void naptarDatum(Integer firstDayName, Integer lastDayOfMonth) {
+        Integer counter = 1;
+        Integer firstDay = firstDayName-1;
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (j < firstDay){
+                    System.out.print("x ");
+                }
+                else if (counter <= lastDayOfMonth) {
+                    System.out.print(counter + " ");
+                    counter++;
+                    firstDay = 0;
+                }
+            }
+            System.out.println("\n");
+        }
+        System.out.println(firstDayName);
+
+
+    }
+
 }
--- Desktop/src/main/java/wv/kalandar/kalandar_projekt/SceneController.java
+++ Desktop/src/main/java/wv/kalandar/kalandar_projekt/SceneController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -14,14 +15,21 @@ import java.io.IOException;
 public class SceneController {
    private Stage stage;
    private Scene scene;
+   Parent root;
 
    //Kalendár nézetbe lépés
     public void switchToCalendar(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("calendar-view.fxml"));
+        root = loader.load();
         FXMLLoader fxmlLoader = new FXMLLoader(CalendarApplication.class.getResource("calendar-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+
+        SceneController sceneController = loader.getController();
+        sceneController.changeButtonText("13");
     }
 
     //Események nézetbe lépés
@@ -41,13 +49,23 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
-
+    //Register nézetbe lépés
     public void switchToRegister(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalendarApplication.class.getResource("register-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML public Button buttonDate0_0;
+    @FXML public Label labelId;
+    @FXML
+    public void changeButtonText(String text) {
+        System.out.println("mukodik");
+        System.out.println(buttonDate0_0.getText());
+        buttonDate0_0.setText(text);
+        System.out.println(buttonDate0_0.getText());
     }
 
 
