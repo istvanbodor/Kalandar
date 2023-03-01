73003f64f0a6242dd0647c9ccb02085baaa93b82
Hoós Roland <shadeeygg@gmail.com>
2023-03-01 14:47:13 +0100

    Writing Comments, deleting unnecessary code

--- Desktop/src/main/java/wv/kalandar/kalandar_projekt/CalendarApplication.java
+++ Desktop/src/main/java/wv/kalandar/kalandar_projekt/CalendarApplication.java
@@ -40,30 +40,30 @@ private Parent root;
         Date date = new Date();
         System.out.println(date);
 
-
+        // Visszaadja a mai dátumot
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(sdf.format(date));
 
+        // Visszaadja a jelenlegi évet
         Integer dateYear = Integer.parseInt(sdf.format(date).split("/")[0]);
         System.out.println(dateYear);
 
+        // Visszaadja a jelenlegi hónapot
         Integer dateMonth = Integer.parseInt(sdf.format(date).split("/")[1]);
         System.out.println(dateMonth);
 
+        // Visszaadja a jelenlegi napot
         Integer dateDay = Integer.parseInt(sdf.format(date).split("/")[2]);
-        System.out.println(dateDay);
-        System.out.println();
 
+        // Visszaadja az utolsó napot INT-ben
         Integer lastDayOfMonth = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
-        System.out.println(lastDayOfMonth);
 
+        //Visszaadja a jelenlegi napot
         Calendar firstDayOfMonth = Calendar.getInstance();
 
-
-        String[] days = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-        System.out.println(firstDayOfMonth.get(Calendar.DAY_OF_WEEK)-1);
-
+        // Visszaadja a jelenlegi hét napját számban, például szerda = 3
         Integer firstDay = firstDayOfMonth.get(Calendar.DAY_OF_WEEK)-1;
+
 
         naptarDatum(firstDay, lastDayOfMonth);
     }
@@ -83,7 +83,7 @@ private Parent root;
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
-                if (j < firstDay){
+                if (j < firstDay){ // Ha a j index kisebb, mint az adott hóna első napja, akkor X-et rak a helyére
                     System.out.print("x ");
                 }
                 else if (counter <= lastDayOfMonth) {
@@ -95,8 +95,6 @@ private Parent root;
             System.out.println("\n");
         }
         System.out.println(firstDayName);
-
-
     }
 
 }
