795e9e8a1d062c843caca1848aa0e2a373270b68
Agrard <gabor.kallai12@gmail.com>
2023-03-07 15:49:38 +0100

    mainly css formating

--- Frontend/src/app/components/MainPage/main-page.component.ts
+++ Frontend/src/app/components/MainPage/main-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit{
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  date = new Date();
 }
