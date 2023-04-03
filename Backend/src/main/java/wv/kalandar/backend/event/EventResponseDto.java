a4a156c0def235b1937172bb9183064d55d31c30
Hoós Roland <shadeeygg@gmail.com>
2023-04-03 09:53:59 +0200

    Giving back the event id once the API events is called

--- Backend/src/main/java/wv/kalandar/backend/event/EventResponseDto.java
+++ Backend/src/main/java/wv/kalandar/backend/event/EventResponseDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class EventResponseDto {
 
+    private long id;
     private String username;
     private String event;
     private String category;
